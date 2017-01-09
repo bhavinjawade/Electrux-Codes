8c371669b549f7f6d3ee4b4e9f37bcba251a6d74
Electrux <ckhandelwal13@gmail.com>
2017-01-09 10:57:42 +0530

    Binary Search Modification

--- BinarySearch/Main.cpp
+++ BinarySearch/Main.cpp
@@ -1,22 +1,27 @@
-#include <iostream>
-#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     system("cls");
     int arr[10] = {10,20,30,40,50,60,70,80,90,100}, lb = 0, ub = (10-1), beg = lb, end = ub, mid = (beg+end)/2, data, pos = -1;
 
-    std::cout << "Enter what to search: ";
-    std::cin >> data;
-    //START BINARY SEARCH
+    printf("Enter what to search: ");
+    scanf("%d", &data);
+    
+	//START BINARY SEARCH
+	
     while (beg <= end) {
-        std::cout << "Array to be searched in for value:" << beg << " " << end  << "\n";
-        for(int i = beg; i <= end; i++)
+        
+		
+		for(int i = beg; i <= end; i++)
         {
-            std::cout << arr[i] << "\t";
+            printf("%d\t", arr[i]);
         }
-        std::cout << "\n";
-        if(arr[mid] == data)
+        
+		printf("\n");
+        
+		if(arr[mid] == data)
         {
             pos = mid + 1;
             break;
@@ -32,16 +37,17 @@ int main()
                 beg = mid + 1;
             }
         }
-        mid = (beg+end)/2;
+        
+		mid = (beg+end)/2;
     }
 
     if(pos == -1)
     {
-        std::cout << "\nElement not found in the array.";
+        printf("\nElement not found in the array.");
     }
     else
     {
-        std::cout << "\nElement found in array at " << pos << "th position.\n";
+        printf("\nElement found in array at %d position.\n", pos);
     }
     system("pause");
     return 0;
