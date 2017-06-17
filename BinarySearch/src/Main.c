9efb44e133ea92b198ded2a664361ad0a97116bf
electrux <ckhandelwal13@gmail.com>
2017-06-17 20:14:42 +0530

    Refactor tabs

--- BinarySearch/src/Main.c
+++ BinarySearch/src/Main.c
@@ -4,55 +4,54 @@
 
 int main()
 {
-    int arr[MAX] = {10,20,30,40,50,60,70,80,90,100},
+	int arr[MAX] = {10,20,30,40,50,60,70,80,90,100},
 		beg = 0,
 		end = MAX - 1,
 		mid = (beg+end)/2,
 		data, pos = -1;
 
-    printf("Enter what to search: ");
-    scanf("%d", &data);
+	printf("Enter what to search: ");
+	scanf("%d", &data);
     
 	//START BINARY SEARCH
-	
-    while (beg <= end) {
-        
-		
+
+	while (beg <= end)
+	{
 		for(int i = beg; i <= end; i++)
-        {
-            printf("%d\t", arr[i]);
-        }
+        	{
+            		printf("%d\t", arr[i]);
+        	}
         
 		printf("\n");
         
 		if(arr[mid] == data)
-        {
-            pos = mid + 1;
-            break;
-        }
-        else
-        {
-            if(data < arr[mid])
-            {
-                end = mid - 1;
-            }
-            else
-            {
-                beg = mid + 1;
-            }
-        }
+		{
+			pos = mid + 1;
+			break;
+		}
+        	else
+        	{
+            		if(data < arr[mid])
+            		{
+                		end = mid - 1;
+            		}
+            		else
+			{
+				beg = mid + 1;
+			}
+		}
         
 		mid = (beg+end)/2;
-    }
+	}
 
-    if(pos == -1)
-    {
-        printf("\nElement not found in the array.");
-    }
-    else
-    {
-        printf("\nElement found in array at %d position.\n", pos);
-    }
+	if(pos == -1)
+	{
+        	printf("\nElement not found in the array.");
+    	}
+    	else
+    	{
+        	printf("\nElement found in array at %d position.\n", pos);
+    	}
 
-    return 0;
+    	return 0;
 }
