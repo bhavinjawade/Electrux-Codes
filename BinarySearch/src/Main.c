c1f04e7485f9e02e0cdf4e449d21a63153d37330
Electrux <ckhandelwal13@gmail.com>
2017-06-17 20:21:38 +0530

    Tab issues solved in Binary Search:

--- BinarySearch/src/Main.c
+++ BinarySearch/src/Main.c
@@ -12,46 +12,46 @@ int main()
 
 	printf("Enter what to search: ");
 	scanf("%d", &data);
-    
+
 	//START BINARY SEARCH
 
 	while (beg <= end)
 	{
 		for(int i = beg; i <= end; i++)
-        	{
-            		printf("%d\t", arr[i]);
-        	}
-        
+		{
+			printf("%d\t", arr[i]);
+		}
+
 		printf("\n");
-        
+
 		if(arr[mid] == data)
 		{
 			pos = mid + 1;
 			break;
 		}
-        	else
-        	{
-            		if(data < arr[mid])
-            		{
-                		end = mid - 1;
-            		}
-            		else
+		else
+		{
+			if(data < arr[mid])
+			{
+				end = mid - 1;
+			}
+			else
 			{
 				beg = mid + 1;
 			}
 		}
-        
+
 		mid = (beg+end)/2;
 	}
 
 	if(pos == -1)
 	{
-        	printf("\nElement not found in the array.");
-    	}
-    	else
-    	{
-        	printf("\nElement found in array at %d position.\n", pos);
-    	}
+		printf("\nElement not found in the array.");
+	}
+	else
+	{
+		printf("\nElement found in array at %d position.\n", pos);
+	}
 
-    	return 0;
+	return 0;
 }
